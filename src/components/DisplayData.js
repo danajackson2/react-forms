b991a299454e3236437017cbc451501ae5e1543d
danajackson2 <danajackson2@gmail.com>
2021-02-03 10:53:29 -0800

    neato

---
+++ src/components/DisplayData.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DisplayData extends React.Component {
+  render() {
+    return (
+      <div>
+          {this.props.sub}
+      </div>
+    )
+  }
+}
+
+export default DisplayData 
